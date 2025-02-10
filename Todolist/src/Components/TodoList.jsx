f30b0534fb500cac510734a174b45d6f26222de1
maureen <maureenotieno38@gmail.com>
2025-02-10 16:25:12 +0300

    complete task added

--- Todolist/src/Components/TodoList.jsx
+++ Todolist/src/Components/TodoList.jsx
@@ -1,7 +1,17 @@
 import Tododisplay from "./Tododisplay";
 import styles from "./TodoList.module.css";
-export default function TodoList({todos,setTodos}){
-   return( <div className={styles.todolist}> {todos.map((item) => (
-    <Tododisplay item={item} key={item} todos={todos} setTodos={setTodos} />
-  ))}</div>);
+export default function TodoList({ todos, setTodos }) {
+  return (
+    <div className={styles.todolist}>
+      {" "}
+      {todos.map((item) => (
+        <Tododisplay
+          item={item}
+          key={item.name}
+          todos={todos}
+          setTodos={setTodos}
+        />
+      ))}
+    </div>
+  );
 }
--- Todolist/src/Components/Tododisplay.jsx
+++ Todolist/src/Components/Tododisplay.jsx
@@ -1,16 +1,31 @@
 import styles from "./Tododisplay.module.css";
-export default function Tododisplay({ item,todos,setTodos }) {
+export default function Tododisplay({ item, todos, setTodos }) {
   function handleDelete(item) {
-    console.log("Delete item",item);
-  setTodos(todos.filter((todo)=>todo!==item));
+    console.log("Delete item", item);
+    setTodos(todos.filter((todo) => todo !== item));
   }
+  function handleClick(name) {
+    // console.log("The item that has been clicked is", name);
+    const newArray = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newArray);
+    // console.log(todos);
+  }
+  const completeDecor = item.done ? styles.completed : "";
   return (
     <div className={styles.display}>
-      <div className={styles.display - item}>
+      <div className={styles["display-item"]}>
         <h3>
-          {item.name}
+          <span
+            className={completeDecor}
+            onClick={() => handleClick(item.name)}
+          >
+            {item.name}
+          </span>
+
           <span>
-            <button onClick={()=>handleDelete(item)} className={styles.btn}>
+            <button onClick={() => handleDelete(item)} className={styles.btn}>
               X
             </button>
           </span>
